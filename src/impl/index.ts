8eab207d30b8af451cd7ccfa3e9cf20a80df0d4c
Goran Mržljak <goran.mrzljak@gmail.com>
2024-06-16 20:56:25 +0200

    fix invalid exports from main index file

--- src/impl/index.ts
+++ src/impl/index.ts
@@ -1,1 +1,5 @@
-export * from './binary-search';
+export * from './binary-search-exact';
+export * from './binary-search-gt';
+export * from './binary-search-gte';
+export * from './binary-search-lt';
+export * from './binary-search-lte';
